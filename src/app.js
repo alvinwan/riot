7ff2324f897ec5557b90f5b4118058b89eb76044
Alvin Wan <alvinwan1@gmail.com>
2018-07-22 19:37:46 -0700

    added homepage statics

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const {app, BrowserWindow} = require('electron')
 
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 1200, height: 800})
 
     // and load the index.html of the app.
     win.loadFile('index.html')
